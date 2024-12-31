2fe1e802e8c860217d72d68b1964750c747edb16
Acid <acydin@hotmail.com>
2024-12-31 14:14:41 +0800

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+-- FX Information
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+
+-- Resource Information
+name 'core_system'
+author 'Acid'
+version '3.5.4'
+description 'Core System By Acid - https://acid.tebex.io/'
